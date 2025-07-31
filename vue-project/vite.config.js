3bb9e649d4518d043fb35d032ec68b2f0073a3e7
zhouchaofei <1479253004@qq.com>
2025-07-31 11:17:55 +0800

    修复wasm问题
    "ESM integration proposal for Wasm" is not supported currently. Use vite-plugin-wasm or other community plugins to handle this. Alternatively, you can use `.wasm?init` or `.wasm?url`. See https://vite.dev/guide/features.html#webassembly for more details.

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import wasm from 'vite-plugin-wasm'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    wasm(),
   ],
   resolve: {
     alias: {
